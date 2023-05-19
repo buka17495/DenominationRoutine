219d10e178307d77b7dea4f3dfc75a25398dd41c
buka17495 <buka6810@gmail.com>
2023-05-19 17:35:36 +0200

    Initial

---
+++ DenominationRoutine/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace DenominationRoutine
+{
+    public class Program
+    {
+        private static readonly int[] Denominations = new int[] { 10, 50, 100 };
+
+        public static int Denominate(int ammount)
+        {
+            var combinations = CalculateCombinations(ammount);
+
+            if (combinations.Count == 0)
+            {
+                Console.WriteLine("No combinations available.");
+            }
+            else
+            {
+                foreach (var combination in combinations)
+                {
+                    var isFirst = true;
+                    foreach (var c in combination)
+                    {
+                        if(!isFirst)
+                            Console.Write(" + ");
+                        Console.Write($"{c.Value} x {c.Key} EUR");
+                        isFirst = false;
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            return combinations.Count;
+        }
+
+        public static List<Dictionary<int, int>> CalculateCombinations(int amount)
+        {
+            var min = Denominations.Min();
+            var dp = new List<Dictionary<int, int>>[amount + 1];
+            dp[0] = new List<Dictionary<int, int>>() { new Dictionary<int, int>() };
+
+            foreach (var denomination in Denominations)
+            {
+                for (var i = denomination; i <= amount; i += min)
+                {
+                    if (dp[i - denomination] != null)
+                    {
+                        foreach (var combination in dp[i - denomination])
+                        {
+                            var newCombination = new Dictionary<int, int>(combination);
+                            if (!newCombination.ContainsKey(denomination))
+                                newCombination[denomination] = 0;
+                            newCombination[denomination]++;
+                            if (dp[i] == null)
+                                dp[i] = new List<Dictionary<int, int>>();
+                            dp[i].Add(newCombination);
+                        }
+                    }
+                }
+            }a
+
+            return dp[amount] ?? new List<Dictionary<int, int>>();
+        }
+    }
+}
---
+++ DenominationRoutineTests/DenominationTests.cs
@@ -0,0 +1,26 @@
+﻿using DenominationRoutine;
+using NUnit.Framework;
+
+namespace DenominationRoutineTests
+{
+    public class DenominationTests
+    {
+        [TestCase(30, 1)]
+        [TestCase(50, 2)]
+        [TestCase(60, 2)]
+        [TestCase(80, 2)]
+        [TestCase(140, 4)]
+        [TestCase(230, 9)]
+        [TestCase(370, 20)]
+        [TestCase(610, 49)]
+        [TestCase(980, 110)]
+        //example
+        [TestCase(100, 4)]
+        public void DenominateTests(int sum, int expectedCount)
+        {
+            var count = Program.Denominate(sum);
+
+            Assert.AreEqual(expectedCount, count);
+        }
+    }
+}
